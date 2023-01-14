ff280d1beee9856a6c16abb419441f0a21968ccd
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-01-14 09:22:02 +0530

    2023 January 06 : 1833. Maximum Ice Cream Bars

---
+++ January/day_06_maximum_ice_cream_bars.cpp
@@ -0,0 +1,12 @@
+//Problem Link : https://leetcode.com/problems/maximum-ice-cream-bars/
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        unordered_map<int,int> freq;
+        for(int element : costs) freq[element]++;
+        
+    }
+};
+
+// 1 1 2 3 4
