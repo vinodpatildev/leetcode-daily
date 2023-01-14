43ecbd603bb7f3554f1cdc80f402f517eeecee90
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-01-14 09:30:36 +0530

    2023 January 11 : 1443. Minimum Time to Collect All Apples in a Tree

---
+++ January/day_11_minimum_time_to_collect_all_apples_in_a_tree.cpp
@@ -0,0 +1,28 @@
+//Problem Link : https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+
+class Solution {
+public:
+    unordered_map <int, vector<int>> graph;
+    unordered_map <int, bool> visited;
+    void genGraph(vector<vector<int>>& edges){
+        for(auto edge:edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+    }
+    int dfs(int node,int cost, vector<bool>& hasApple){
+        if(visited[node]) return 0;
+        visited[node] = true;
+
+        int localCost=0;
+        for(auto neighbor:graph[node]){
+            localCost += dfs(neighbor,2,hasApple);
+        }
+        if(localCost==0 && !hasApple[node]) return 0;
+        return cost+localCost;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        genGraph(edges);
+        return dfs(0,0,hasApple);
+    }
+};
